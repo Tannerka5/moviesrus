3a1e55b4aec2c61648f2e9c5e737603d8de4489b
Tannerka5 <38678080+Tannerka5@users.noreply.github.com>
2022-12-12 00:13:57 -0700

    Attempt #6?

--- moviesrus/settings.py
+++ moviesrus/settings.py
@@ -26,12 +26,12 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-CSRF_TRUSTED_ORIGINS = ['web-production-3595.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3595.up.railway.app']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", 'web-production-3595.up.railway.app']
+ALLOWED_HOSTS = ["*", 'https://web-production-3595.up.railway.app']
 
 
 # Application definition
