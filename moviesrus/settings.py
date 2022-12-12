c173af70cb1b256576400006842623373b9522cf
Tannerka5 <38678080+Tannerka5@users.noreply.github.com>
2022-12-12 00:01:13 -0700

    Attempt #3

--- moviesrus/settings.py
+++ moviesrus/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3595.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
