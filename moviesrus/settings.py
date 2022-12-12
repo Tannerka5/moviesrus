2b36146554a2649e190a3793eda6fe83ce291a2d
Tannerka5 <38678080+Tannerka5@users.noreply.github.com>
2022-12-11 23:49:53 -0700

    Add database

--- moviesrus/settings.py
+++ moviesrus/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,15 +79,10 @@ WSGI_APPLICATION = 'moviesrus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myplex',
-        'USER': 'postgres',
-        'PASSWORD': 'Tatkinson5',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
