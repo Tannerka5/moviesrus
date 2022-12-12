e275a0bad3c7c8a477ea3a3ed28d468f3144973f
Tannerka5 <38678080+Tannerka5@users.noreply.github.com>
2022-12-12 00:07:21 -0700

    Attempt #5

--- moviesrus/settings.py
+++ moviesrus/settings.py
@@ -31,7 +31,7 @@ CSRF_TRUSTED_ORIGINS = ['web-production-3595.up.railway.app']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", 'web-production-3595.up.railway.app']
 
 
 # Application definition
